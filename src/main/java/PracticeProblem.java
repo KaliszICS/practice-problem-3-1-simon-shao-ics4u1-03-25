08526382cec99bfd17c30c5d96ea95f3a8e42318
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-04-01 17:21:59 +0000

    Complete Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,23 @@ public class PracticeProblem {
 
 	}
 
+public static int sumOfDigits(int x){
+	x = Math.abs(x);
+if (x<10){
+	return x;
+}
+
+	return sumOfDigits(x/10) + sumOfDigits(x%10);
+}
 	
+public static int factorial(int n){
+	if (n == 0){
+		return 1;
+	}
+if (n<0){
+	return 0;
+}
+	return factorial(n-1)*n;
+}
 
 }
